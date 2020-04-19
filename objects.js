5f57a1e8350a63d6844dfdbc37b5548904974223
puthran28 <61663185+puthran28@users.noreply.github.com>
2020-04-19 12:01:00 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,89 @@
+
+class Board {
+  constructor(grid) {
+    this.grid = [
+      [new Square(), new Square(), new Square()],
+      [new Square(), new Square(), new Square()],
+      [new Square(), new Square(), new Square()]
+    ];
+  }
+
+  isFull() {
+    let count = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (this.grid[i][j].state != "") { count++ }
+      }
+    }
+    if (count == 9) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  check_win() {
+
+    for (let i = 0; i < 3; i++) {
+      if (this.grid[i][0].state == this.grid[i][1].state && this.grid[i][0].state == this.grid[i][2].state && this.grid[i][0].state != "") {
+        print_winner();
+      }
+    }
+
+    for (let j = 0; j < 3; j++) {
+      if (this.grid[0][j].state == this.grid[1][j].state && this.grid[0][j].state == this.grid[2][j].state && this.grid[0][j].state != "") {
+        print_winner();
+      }
+    }
+
+    if ((this.grid[0][0].state == this.grid[1][1].state && this.grid[0][0].state == this.grid[2][2].state && this.grid[0][0].state != "") ||
+      (this.grid[0][2].state == this.grid[1][1].state && this.grid[0][2].state == this.grid[2][0].state && this.grid[0][2].state != "")) {
+      print_winner();
+    }
+
+    if (this.isFull() && this.check_win()==false) {
+      print_tie();
+    }
+
+  }
+}
+
+
+
+class Square {
+  constructor(state) {
+    this.state = "";
+  }
+
+  whoseTurn() {
+    if (counter == 0) {
+      setMessage("O's turn")
+      return this.state = "x";
+    }
+    else {
+        setMessage("X's turn")
+      return this.state = "o";
+    }
+  }
+}
+
+
+
+class Player {
+  constructor(symbol) {
+    this.symbol = symbol;
+  }
+}
+
+
+
+class Game {
+  constructor() {
+    this.board = new Board();
+    this.players = [
+      new Player("x"),
+      new Player("o")
+    ];
+  }
+}
